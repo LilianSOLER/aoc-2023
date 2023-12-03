dba4c3d7853526bb945ff86bc5e6ea7e3caec010
Lilian SOLER <soler.lilian64@gmail.com>
2023-12-03 13:29:15 +0100

    :sparkles: Add day 2 - part 2

--- week-1/day-2/day-2.py
+++ week-1/day-2/day-2.py
@@ -27,13 +27,34 @@ def is_valid_game(game: str, condition: dict[str, int] = None) -> int:
     return game_id
 
 
+def min_cond_to_win(game: str) -> int:
+    game_info = game.split(":")
+    game_id = int(game_info[0].split(" ")[1])
+
+    games = game_info[1].split(";")
+    cubes_dict = []
+
+    for game in games:
+        cubes_dict.append({cube_info[1]: int(cube_info[0]) for cube in game.strip().split(",") for cube_info in
+                           [cube.strip().split(" ")]})
+
+    condition = {"red": -1, "green": -1, "blue": -1}
+
+    for cube_dict in cubes_dict:
+        for key in cube_dict:
+            if cube_dict[key] > condition[key] or condition[key] == -1:
+                condition[key] = cube_dict[key]
+
+    return condition["red"] * condition["green"] * condition["blue"]
+
+
 def part_1(content: List[str]) -> int:
     conditions = {"red": 12, "green": 13, "blue": 14}
     return sum([is_valid_game(game, conditions) if game else 0 for game in content])
 
 
 def part_2(content: List[str]) -> int:
-    return 0
+    return sum([min_cond_to_win(game) for game in content])
 
 
 def main(file_path: str) -> None:
